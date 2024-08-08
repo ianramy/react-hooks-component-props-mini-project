e2a04fd42e37989abd6e6a1023d5493a9777d289
ianramy <imwagore@gmail.com>
2024-08-08 03:35:11 +0300

    origin/master

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-import React from "react";
-import blogData from "../data/blog";
-
-console.log(blogData);
+import React from 'react';
+import Header from './Header';
+import About from './About';
+import ArticleList from './ArticleList';
+import blogData from '../data/blog'; // Assuming this imports the posts data
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name="Underreacted" />
+      <About 
+        image="https://via.placeholder.com/215" // Default image URL
+        about="About this blog" 
+      />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
